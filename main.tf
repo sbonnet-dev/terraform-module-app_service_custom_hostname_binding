af0493e26f891612b4fe6d5886f3dea4814a0581
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-11 15:56:41 +0200

    fixed main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_custom_hostname_binding" "res-18" {
+resource "azurerm_app_service_custom_hostname_binding" "app_service_custom_hostname_binding" {
   app_service_name    = var.app_service_name                                                //"app-backend-tvxbgsftoyot5"
   hostname            = lower(format("%s.azurewebsites.net",  var.app_service_name))       //app-backend-tvxbgsftoyot5.azurewebsites.net"
 
